9c124fb0b3da3d79f61e67b0a01947d88deeac67
jiandi1234567890 <550392461@qq,com>
2016-03-01 11:23:01 +0800

    Initial Commit

---
+++ UIView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIView
+//
+//  Created by 张海禄 on 16/3/1.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView
+//
+//  Created by 张海禄 on 16/3/1.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
